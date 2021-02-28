ae2a3aa3f26669659b5b33f78a86d1cd310fa9ab
happieChappie <sidharthad@protonmail.com>
2021-02-27 21:29:36 -0500

    working transfer from in the tests

--- contracts/Escrow.sol
+++ contracts/Escrow.sol
@@ -1,5 +1,6 @@
 //SPDX-License-Identifier: Unlicense
 pragma solidity ^0.7.0;
+import "hardhat/console.sol";
 
 import "./interfaces/IERC20.sol";
 contract Escrow {
@@ -11,10 +12,11 @@ contract Escrow {
     // aave interest bearing DAI
     // IERC20 aDai = IERC20(0x028171bCA77440897B824Ca71D1c56caC55b68A3);
     // the DAI stablecoin 
-    // IERC20 dai = IERC20(0x6B175474E89094C44Da98b954EedeAC495271d0F);
 	// address public USDTAddress = 0xdAC17F958D2ee523a2206206994597C13D831ec7;
 	// address USDTAddress = 0x6b175474e89094c44da98b954eedeac495271d0f;
+	// IERC20 usdt = IERC20(0xdAC17F958D2ee523a2206206994597C13D831ec7);
 	IERC20 usdt = IERC20(0xdAC17F958D2ee523a2206206994597C13D831ec7);
+    // IERC20 usdt = IERC20(0x6B175474E89094C44Da98b954EedeAC495271d0F);
 
     constructor(address _arbiter, address _beneficiary, uint _amount) {
         arbiter = _arbiter;
@@ -26,7 +28,13 @@ contract Escrow {
     }
 
     function approve() external {
-        usdt.transferFrom(depositor, address(this), initialBalance);
-    }
-}
+		console.log("===============");
+		console.log(address(this));
+		// console.log(usdt);
+        // usdt.transfer(address(this), initialBalance);
+        usdt.transferFrom(msg.sender, address(this), 200);
 
+    }
+	/*
+	*/
+}
