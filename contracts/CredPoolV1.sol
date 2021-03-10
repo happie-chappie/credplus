c047f516f661e8fa4e1227e133fdfe57138c20a2
happieChappie <sidharthad@protonmail.com>
2021-03-10 16:06:27 -0500

    using openzeppelin 0.8.0 contracts

--- contracts/CredPoolV1.sol
+++ contracts/CredPoolV1.sol
@@ -1,7 +1,7 @@
 //SPDX-License-Identifier: Unlicense
 pragma solidity ^0.8.0;
 import "hardhat/console.sol";
-import "./interfaces/IERC20.sol";
+import "./dependencies/IERC20.sol";
 
 contract CredPoolV1 {
 	// fixed interest rate
--- contracts/CredPoolV2.sol
+++ contracts/CredPoolV2.sol
@@ -1,7 +1,8 @@
 //SPDX-License-Identifier: Unlicense
 pragma solidity ^0.8.0;
 import "hardhat/console.sol";
-import "./interfaces/IERC20.sol";
+// import "./interfaces/IERC20.sol";
+import "./dependencies/IERC20.sol";
 // import "
 
 contract CredPoolV2 {
