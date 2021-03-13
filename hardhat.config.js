8b037be868e75f09a3931dcc7de03734fdd7cf55
happieChappie <sidharthad@protonmail.com>
2021-03-13 08:54:48 -0500

    got the kovan deployment workflows up

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,9 @@
 require("@nomiclabs/hardhat-waffle");
 require('dotenv').config();
 
+const INFURA_PROJECT_ID = process.env.INFURA_PROJECT_ID;
+const ROPSTEN_PRIVATE_KEY = process.env.ROPSTEN_PRIVATE_KEY;
+
 // The next line is part of the sample project, you don't need it in your
 // project. It imports a Hardhat task definition, that can be used for
 // testing the frontend.
@@ -13,9 +16,13 @@ module.exports = {
       forking: {
 	url: process.env.FORKING_URL,
 	blockNumber: 11941398
+	// gas: 95000000,
+	// blockGasLimit: 950000000
       },
-      // gas: 95000000,
-      // blockGasLimit: 950000000
+    },
+    kovan: {
+      url: `https://kovan.infura.io/v3/${INFURA_PROJECT_ID}`,
+      accounts: [`0x${ROPSTEN_PRIVATE_KEY}`]
     }
   },
   mocha: {
